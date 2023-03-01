d82a591c00fae037acecd4bce384e827efec07d3
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-01 16:29:12 -0600

    1629

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,15 +1,11 @@
 SELECT * FROM albums;
-UPDATE albums
-SET sales = sales * 10;
+UPDATE albums SET sales = sales * 10;
 SELECT * FROM albums;
 
 SELECT * FROM albums WHERE release_date < 1980;
-UPDATE albums
-SET release_date = release_date + 100;
-SELECT * FROM albums WHERE release_date > 2000;
+UPDATE albums SET release_date = release_date - 100 WHERE release_date < 1980;
+SELECT * FROM albums WHERE release_date < 1900;
 
 SELECT * FROM albums WHERE artist = 'Michael Jackson';
-UPDATE albums
-SET artist = 'Peter Jackson'
-WHERE artist = 'Michael Jackson';
+UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
 SELECT * FROM albums WHERE artist = 'Peter Jackson';
