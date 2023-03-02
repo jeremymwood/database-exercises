cbc99819b3b0df320a2a784919b643c95c2c86e0
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-02 11:57:05 -0600

    1157

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -9,23 +9,28 @@ FROM employees
 ORDER BY last_name DESC, first_name ASC;
 
 # EXERCISES
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+SELECT * FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 ORDER BY first_name;
 
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+SELECT * FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 ORDER BY first_name, last_name;
 
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+SELECT * FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 ORDER BY last_name, first_name;
 
 SELECT *
 FROM employees
 WHERE last_name LIKE 'E%'
+    OR last_name LIKE '%e'
 ORDER BY emp_no;
 
 SELECT *
 FROM employees
 WHERE last_name LIKE 'E%'
+   OR last_name LIKE '%e'
 ORDER BY emp_no DESC;
 
 SELECT *
