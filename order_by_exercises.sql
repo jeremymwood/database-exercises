be0f22342840439ec398402d4adf9e5a6c087838
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-02 11:38:25 -0600

    1138

---
+++ order_by_exercises.sql
@@ -0,0 +1,29 @@
+use ymir_employees;
+
+SELECT first_name, last_name
+FROM employees
+ORDER BY last_name;
+
+SELECT first_name, last_name
+FROM employees
+ORDER BY last_name DESC, first_name ASC;
+
+# EXERCISES
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name;
+
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
+
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no DESC;
--- where_exercises.sql
+++ where_exercises.sql
@@ -12,7 +12,11 @@ WHERE last_name LIKE '%q%';
 
 SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya') OR first_name IN ('Maya');
 
-SELECT * FROM employees WHERE (first_name IN ('Irena', 'Vidya') OR first_name IN ('Maya')) AND gender = 'M';
+SELECT * FROM employees
+     WHERE (first_name IN ('Irena')
+            OR first_name IN ('Vidya')
+            OR first_name IN ('Maya'))
+       AND gender = 'M';
 
 SELECT *
 FROM employees
@@ -24,4 +28,4 @@ WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
 
 SELECT *
 FROM employees
-WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
