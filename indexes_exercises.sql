1f05d3961b56e86d50d95de0878396e34a31a57a
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-03 10:43:09 -0600

    1043

--- indexes_exercises.sql
+++ indexes_exercises.sql
@@ -51,3 +51,38 @@ ALTER TABLE albums
     ADD UNIQUE (album_name);
 
 describe ymir_jeremy.albums;
+
+# right click ymir_jeremy, new query console, paste and run
+# CREATE TABLE roles (
+#                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+#                        name VARCHAR(100) NOT NULL,
+#                        PRIMARY KEY (id)
+# );
+#
+# CREATE TABLE users (
+#                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+#                        name VARCHAR(100) NOT NULL,
+#                        email VARCHAR(100) NOT NULL,
+#                        role_id INT UNSIGNED DEFAULT NULL,
+#                        PRIMARY KEY (id),
+#                        FOREIGN KEY (role_id) REFERENCES roles (id)
+# );
+#
+# INSERT INTO roles (name) VALUES ('admin');
+# INSERT INTO roles (name) VALUES ('author');
+# INSERT INTO roles (name) VALUES ('reviewer');
+# INSERT INTO roles (name) VALUES ('commenter');
+#
+# INSERT INTO users (name, email, role_id) VALUES
+#      ('bob', 'bob@example.com', 1),
+#      ('joe', 'joe@example.com', 2),
+#      ('sally', 'sally@example.com', 3),
+#      ('adam', 'adam@example.com', 3),
+#      ('jane', 'jane@example.com', null),
+#      ('mike', 'mike@example.com', null);
+
+describe users;
+
+SELECT *
+FROM users
+WHERE role_id IS NOT NULL;
