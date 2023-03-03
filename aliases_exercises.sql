82b63b4aa53c15da5e1c55dbd69348483202ffc7
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-02 19:39:16 -0600

    1939

---
+++ aliases_exercises.sql
@@ -0,0 +1,19 @@
+use ymir_employees;
+
+SELECT CONCAT(last_name, ', ', first_name) AS full_name
+FROM employees
+GROUP BY full_name, last_name
+ORDER BY last_name
+LIMIT 10;
+
+SELECT CONCAT(last_name, ', ', first_name) AS full_name, birth_date as DOB
+FROM employees
+GROUP BY full_name, last_name
+ORDER BY last_name
+LIMIT 10;
+
+SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name) AS full_name, birth_date AS DOB
+FROM employees
+GROUP BY full_name, last_name
+ORDER BY last_name
+LIMIT 10;
