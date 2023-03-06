3044ba93f18e9df9fdc274f7aadfd32f973dd455
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-06 11:22:03 -0600

    1122

--- adlister.sql
+++ adlister.sql
@@ -1,17 +1,23 @@
 #AdLister: small scale Craigslist clone
-#users can post ads for items
-#
 # Specs:
-# users
+# adUsers
+    # id
     # email
     # password
 # ads
+    # id
     # title
     # description
-# item
-    # title
-    # category ("help wanted", "giveaway", or "furniture")
+    # user id
+    # foreign key
+# category ("help wanted", "giveaway", or "furniture")
+    # id
 
+# ads_categories
+    # ad_id
+    # category_id
+    # foreign key
+    # foreign key
 
 use ymir_jeremy;
 
@@ -24,25 +30,19 @@ CREATE TABLE adUsers (
 
 INSERT INTO adUsers(email, password)
 VALUES
-    ('jeremy@abc.com', 'P@ssw0rd'),
-    ('kenzi@abc.com', 'P@ssw0rd'),
-    ('nikki@abc.com', 'P@ssw0rd'),
-    ('matty@abc.com', 'P@ssw0rd'),
-    ('rosie@abc.com', 'P@ssw0rd'),
-    ('matt@abc.com', 'P@ssw0rd');
+    ('jeremy@abc.com', 'P@ssw0rd1'),
+    ('kenzi@abc.com', 'P@ssw0rd2'),
+    ('nikki@abc.com', 'P@ssw0rd3')
+;
 
 describe adUsers;
 select *
 from adUsers;
 
-# Users create ads with:
-    # title
-    # description
-
 CREATE TABLE ads (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     title VARCHAR(200) NOT NULL,
-    description VARCHAR(1000) NOT NULL,
+    description text NOT NULL,
     aduser_id INT UNSIGNED NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (aduser_id) REFERENCES adUsers (id)
@@ -53,10 +53,40 @@ CREATE TABLE ads (
 # VALUES ((select id from authors where first_name = 'Douglas' and last_name = 'Adams'),
 #         'I love deadlines. I love the whooshing noise they make as they go by.');
 INSERT INTO ads (aduser_id, title, description)
-VALUES ((select id from adUsers where email = 'jeremy@abc.com' and password = 'P@ssw0rd'), '1963 Aston Martin DB5, 1.3M OBO', 'No, Mr. Bond, I expect you to drive!');
+VALUES ((
+    select id
+    from adUsers
+    where email = 'jeremy@abc.com'
+    and password = 'P@ssw0rd1'),
+        '1963 Aston Martin DB5, 1.3M OBO',
+        'No, Mr. Bond, I expect you to drive!  Pristine condition, 20k miles, original engine, restored interior.');
 
 INSERT INTO ads (aduser_id, title, description)
-VALUES ((select id from adUsers where email = 'jeremy@abc.com' and password = 'P@ssw0rd'), 'Box of assorted rocks, $7 FIRM', 'These are some of my favorite rocks and i just need the space in my garage.');
+VALUES ((
+    select id
+    from adUsers
+    where email = 'jeremy@abc.com'
+    and password = 'P@ssw0rd1'),
+        'Box of assorted rocks, $7 FIRM',
+        'These are some of my favorite rocks and i just need the space in my garage.');
+
+INSERT INTO ads (aduser_id, title, description)
+VALUES ((
+    select id
+    from adUsers
+    where email = 'kenzi@abc.com'
+    and password = 'P@ssw0rd2'),
+        'Prom dress, $200 OBO',
+        'Budget conscious option.');
+
+INSERT INTO ads (aduser_id, title, description)
+VALUES ((
+    select id
+    from adUsers
+    where email = 'nikki@abc.com'
+    and password = 'P@ssw0rd3'),
+        'Labradoodle puppies, AKC/full vax, $400 M/F',
+        'Great birthday present, AKC certified, fully vaccinated and spayed/neutered.');
 
 select *
 from ads;
@@ -64,36 +94,40 @@ from ads;
 # An ad can be in one or more categories
 # (for example, "help wanted", "giveaway", or "furniture")
 
-CREATE TABLE adCategories (
+CREATE TABLE categories (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(20) NOT NULL,
-    PRIMARY KEY (id)
+    ad_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (ad_id) REFERENCES ads (id)
 );
 
-INSERT INTO adCategories(name) VALUES
-#     ('Help wanted'),
-#     ('Giveaway'),
-#     ('Furniture'),
-#     ('Autos')
-    ('Hot deals');
+INSERT INTO categories(name) VALUES
+    ('Cars and trucks'),
+    ('Furniture'),
+    ('Giveaway'),
+    ('Hot deals'),
+    ('Housing'),
+    ('Jobs')
+    ;
 
-describe adCategories;
+describe categories;
 
 select *
-from adCategories;
+from categories;
 
-DELETE FROM adCategories
-WHERE ID > 4;
+# DELETE FROM categories
+# WHERE ID > 4;
 
 # doc, how can i renumber from console?
-# update adCategories
-# set adCategories.id to 5 where = adCategories.id = 9;
+# update categories
+# set categories.id to 5 where = categories.id = 9;
 
 CREATE TABLE ads_categories (
     ad_id INTEGER UNSIGNED NOT NULL,
     category_id INTEGER UNSIGNED NOT NULL,
     FOREIGN KEY (ad_id) REFERENCES ads(id),
-    FOREIGN KEY (category_id) REFERENCES adCategories(id)
+    FOREIGN KEY (category_id) REFERENCES categories(id)
 );
 
 INSERT INTO ads_categories(ad_id, category_id)
