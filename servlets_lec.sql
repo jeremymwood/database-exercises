4ba01f3b7179588ad9e34af779100fd66170a080
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-07 11:27:46 -0600

    1127

---
+++ servlets_lec.sql
@@ -0,0 +1,11 @@
+# backend:
+# server side program that processes and delivers response to host/client/browser request
+# can be served up in many languages: java w/ tomcat/glassfish etc, js w/ node, python, c#, etc.
+# request can be for website, files, api's...
+# apache, engine x, etc. for server will communicate with database
+# servlets are bulky, spring will handle this analogue process.  use spring for restful api's
+# servlets handle requests and responses
+# css and js folders can be dropped directly into src/webapps folder
+# html can be pasted into jsp, juts include boilerplate items
+# out.print() for browser display
+# sout for serverside display
